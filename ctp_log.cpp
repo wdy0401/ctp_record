77e4a218ba16633922e704252af8e3c77526953e
wdy0401 <wdy0401@gmail.com>
2015-12-27 23:41:32 +0800

    增加数据类型  尚未测试

--- ctp_log.cpp
+++ ctp_log.cpp
@@ -24,13 +24,18 @@ void ctp_log::writeinfo(CThostFtdcDepthMarketDataField *pDepthMarketData)
     os<< "," << pDepthMarketData->BidVolume1;
     os<< "," << pDepthMarketData->AskVolume1;
     os<< "," << pDepthMarketData->LastPrice;
-    os<< "," << pDepthMarketData->HighestPrice;
-    os<< "," << pDepthMarketData->LowestPrice;
-    os<< "," << pDepthMarketData->Turnover;
-    os<< "," << pDepthMarketData->AveragePrice;
-    os<< "," << pDepthMarketData->PreSettlementPrice;
-    // SettlementPrice 是无效数据
-    //os<< "," << pDepthMarketData->SettlementPrice;
+    os<< "," << pDepthMarketData->AveragePrice;//当日均价
+    os<< "," << pDepthMarketData->Turnover;//成交金额
+    os<< "," << pDepthMarketData->Volume;//数量
+    os<< "," << pDepthMarketData->OpenInterest;//持仓量
+    os<< "," << pDepthMarketData->OpenPrice;//今开
+    os<< "," << pDepthMarketData->HighestPrice;//今高
+    os<< "," << pDepthMarketData->LowestPrice;//今低
+    os<< "," << pDepthMarketData->UpperLimitPrice;//涨停板价格
+    os<< "," << pDepthMarketData->LowerLimitPrice;//跌停板价格
+    os<< "," << pDepthMarketData->PreSettlementPrice;//昨结算
+    os<< "," << pDepthMarketData->PreClosePrice;//昨收盘
+    os<< "," << pDepthMarketData->PreOpenInterest;//昨持仓
     os<< endl;
     outlog<<os.str();
     emit broadcast_quote(os.str());
