9e7980b68343b07d28b15b30f2dfeba2e9fc19d5
wdy0401 <wdy0401@gmail.com>
2016-06-03 20:57:46 +0800

    option

--- ctp_quote.cpp
+++ ctp_quote.cpp
@@ -33,24 +33,24 @@ void ctp_quote::init()
 {
     pUserApi=CThostFtdcMdApi::CreateFtdcMdApi();
     nRequestID=0;
-	nppInstrumentID=0;
-	ctp_time_length=sizeof(TThostFtdcTimeType);
-	
-	req=new CThostFtdcReqUserLoginField;
-	memset(req, 0, sizeof(*req));
-	strncpy(req->BrokerID,const_cast<char*>(simu_cfg.getparam("BROKER_ID").c_str()),sizeof(req->BrokerID));
-	strncpy(req->UserID,const_cast<char*>(simu_cfg.getparam("INVESTOR_ID").c_str()),sizeof(req->UserID));
-	strncpy(req->Password,const_cast<char*>(simu_cfg.getparam("PASSWORD").c_str()),sizeof(req->Password));
+    nppInstrumentID=0;
+    ctp_time_length=sizeof(TThostFtdcTimeType);
+
+    req=new CThostFtdcReqUserLoginField;
+    memset(req, 0, sizeof(*req));
+    strncpy(req->BrokerID,const_cast<char*>(simu_cfg.getparam("BROKER_ID").c_str()),sizeof(req->BrokerID));
+    strncpy(req->UserID,const_cast<char*>(simu_cfg.getparam("INVESTOR_ID").c_str()),sizeof(req->UserID));
+    strncpy(req->Password,const_cast<char*>(simu_cfg.getparam("PASSWORD").c_str()),sizeof(req->Password));
 
 
-	ppInstrumentID=new char * [MAX_CONTRACT_NUMBER];
-	list<string> contracts=wfunction::splitstring(simu_cfg.getparam("INSTRUMENT_ID"));
-	for(list<string>::iterator iter=contracts.begin();iter!=contracts.end();iter++)
-	{
-		ppInstrumentID[nppInstrumentID]=new char [MAX_CONTRACT_NAME];
-		memset(ppInstrumentID[nppInstrumentID],'\0',MAX_CONTRACT_NAME);
-		strncpy(ppInstrumentID[nppInstrumentID],iter->c_str(),iter->size());
-		nppInstrumentID++;
+    ppInstrumentID=new char * [MAX_CONTRACT_NUMBER];
+    list<string> contracts=wfunction::splitstring(simu_cfg.getparam("INSTRUMENT_ID"));
+    for(list<string>::iterator iter=contracts.begin();iter!=contracts.end();iter++)
+    {
+        ppInstrumentID[nppInstrumentID]=new char [MAX_CONTRACT_NAME];
+        memset(ppInstrumentID[nppInstrumentID],'\0',MAX_CONTRACT_NAME);
+        strncpy(ppInstrumentID[nppInstrumentID],iter->c_str(),iter->size());
+        nppInstrumentID++;
         cout<<"INFO: quote contract   "<<iter->c_str()<<endl;
     }
     cout << "INFO: BrokerID: "<<req->BrokerID<<endl;
@@ -62,63 +62,71 @@ void ctp_quote::login()
 {
     pUserApi->RegisterSpi(this);
     pUserApi->RegisterFront(const_cast<char*>(simu_cfg.getparam("QUOTE_FRONT_ADDR").c_str()));
-	pUserApi->Init();
-	pUserApi->Join();
+    pUserApi->Init();
+    pUserApi->Join();
 }
 void ctp_quote::ReqUserLogin()
 {
-	int iResult = pUserApi->ReqUserLogin(req, ++nRequestID);
-	cout << "--->>> 发送用户登录请求: " << ((iResult == 0) ? "成功" : "失败") << endl;
+    int iResult = pUserApi->ReqUserLogin(req, ++nRequestID);
+    cout << "--->>> 发送用户登录请求: " << ((iResult == 0) ? "成功" : "失败") << endl;
 }
 
 void ctp_quote::OnRspError(CThostFtdcRspInfoField *pRspInfo,int nRequestID, bool bIsLast)
 {
-	//尚不清楚在具体哪种情况下会触发此回调
-	cout<< "--->>> "<< "OnRspError\t RequeseID="<<nRequestID<< endl;
-	IsErrorRspInfo(pRspInfo);
+    //尚不清楚在具体哪种情况下会触发此回调
+    cout<< "--->>> "<< "OnRspError\t RequeseID="<<nRequestID<< endl;
+    IsErrorRspInfo(pRspInfo);
 }
 void ctp_quote::OnFrontDisconnected(int nReason)
 {
-	cout << "--->>> " << "OnFrontDisconnected" << endl;
-	cout << "Reasons\n\t0x1001 网络读失败\n\t0x1002 网络写失败\n\t0x2001 接收心跳超时\n\t0x2002 发送心跳失败\n\t0x2003 收到错误报文"<<endl;
-	cout << "\tReason = " << nReason << endl;
+    cout << "--->>> " << "OnFrontDisconnected" << endl;
+    cout << "Reasons\n\t0x1001 网络读失败\n\t0x1002 网络写失败\n\t0x2001 接收心跳超时\n\t0x2002 发送心跳失败\n\t0x2003 收到错误报文"<<endl;
+    cout << "\tReason = " << nReason << endl;
 }
 
 ///当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
 void ctp_quote::OnFrontConnected()
 {
-	cout << "--->>> " << "OnFrontConnected" << endl;
-	ReqUserLogin();
+    cout << "--->>> " << "OnFrontConnected" << endl;
+    ReqUserLogin();
 }
-	
+
 ///登录请求响应
 void ctp_quote::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,	CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
 {
-	cout << "--->>> " << "OnRspUserLogin" << endl;
-	if (bIsLast && !IsErrorRspInfo(pRspInfo))
-	{
-		///获取当前交易日
-		cout << "--->>> 获取当前交易日 = " << pUserApi->GetTradingDay()<< endl;
-		// 请求订阅行情
-		SubscribeMarketData();
-	}
+    cout << "--->>> " << "OnRspUserLogin" << endl;
+    if (bIsLast && !IsErrorRspInfo(pRspInfo))
+    {
+        ///获取当前交易日
+        cout << "--->>> 获取当前交易日 = " << pUserApi->GetTradingDay()<< endl;
+        // 请求订阅行情
+        SubscribeMarketData();
+    }
 }
 void ctp_quote::SubscribeMarketData()
 {
-	int iResult = pUserApi->SubscribeMarketData(ppInstrumentID, nppInstrumentID);
-	cout << "--->>> 发送行情订阅请求: " << ((iResult == 0) ? "成功" : "失败") << endl;
+    int iResult;
+    if(nppInstrumentID > 0)
+    {
+       iResult = pUserApi->SubscribeMarketData(ppInstrumentID, nppInstrumentID);
+    }
+    else
+    {
+        iResult = pUserApi->SubscribeMarketData(nullptr,0);
+    }
+    cout << "--->>> 发送行情订阅请求: " << ((iResult == 0) ? "成功" : "失败") << endl;
 }
 
 ///订阅行情应答
 void ctp_quote::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
 {
-	cout << "OnRspSubMarketData" << endl;
+    cout << "OnRspSubMarketData" << endl;
 }
 
 ///取消订阅行情应答
 void ctp_quote::OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast)
 {
-	cout << "OnRspUnSubMarketData" << endl;
+    cout << "OnRspUnSubMarketData" << endl;
 }
 
 ///深度行情通知
@@ -131,10 +139,10 @@ void ctp_quote::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *p)
 }
 bool ctp_quote::IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo)
 {
-	bool bResult = ((pRspInfo) && (pRspInfo->ErrorID != 0));
-	if (bResult)
-		cout << "--->>> ErrorID=" << pRspInfo->ErrorID << ", ErrorMsg=" << pRspInfo->ErrorMsg << endl;
-	return bResult;
+    bool bResult = ((pRspInfo) && (pRspInfo->ErrorID != 0));
+    if (bResult)
+        cout << "--->>> ErrorID=" << pRspInfo->ErrorID << ", ErrorMsg=" << pRspInfo->ErrorMsg << endl;
+    return bResult;
 }
 char *ctp_quote::mk_quote_con_dir()
 {
--- ctp_record.pro
+++ ctp_record.pro
@@ -20,22 +20,22 @@ SOURCES += main.cpp \
     ctp_quote_qthread.cpp \
     ctp_manager.cpp \
     ctp_log.cpp \
-	../gpp_qt/cfg/cfg.cpp \
-	../gpp_qt/log_info/log_info.cpp \
-	../gpp_qt/wfunction/wfunction.cpp \
-	../gpp_qt/wfunction/udp_sender.cpp \
+        ../gpp_qt/cfg/cfg.cpp \
+        ../gpp_qt/log_info/log_info.cpp \
+        ../gpp_qt/wfunction/wfunction.cpp \
+        ../gpp_qt/wfunction/udp_sender.cpp \
 
 HEADERS += \
     ctp_quote.h \
     ctp_quote_qthread.h \
     ctp_manager.h \
     ctp_log.h \
-	../gpp_qt/cfg/cfg.h \
+        ../gpp_qt/cfg/cfg.h \
     ../gpp_qt/log_info/log_info.h \
-	../gpp_qt/wfunction/wfunction.h \
-	../gpp_qt/wfunction/udp_sender.h \
+        ../gpp_qt/wfunction/wfunction.h \
+        ../gpp_qt/wfunction/udp_sender.h \
 
-win32: LIBS += -L$$PWD/../libs/ctp/ -lthostmduserapi
+win32: LIBS += -L$$PWD/../libs/ctp_option/ -lthostmduserapi
 
-INCLUDEPATH += $$PWD/../libs/ctp
-DEPENDPATH += $$PWD/../libs/ctp
+INCLUDEPATH += $$PWD/../libs/ctp_option
+DEPENDPATH += $$PWD/../libs/ctp_option
